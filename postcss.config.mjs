3d76235066e4dad3094a14be3634b5fc9cd1b411
atabidze <98154053+atabidze@users.noreply.github.com>
2025-10-09 16:13:32 +0400

    4444

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
-/** @type {import('postcss').Config} */
+/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 }
